90b3072b1f653526ef96dc6a1e51a008265375b4
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-09-29 19:55:35 +0700

    Operator Method

--- 11.operasi_&_manupulasi_string.py
+++ 11.operasi_&_manupulasi_string.py
@@ -45,3 +45,72 @@ print(chr(data))
 data = "otong surotong pararotong"
 jumlah = data.count("o")
 print(jumlah)
+
+# # merubah case dari string
+
+# merubah semua ke uppercase
+salam = "brO"
+salam = salam.upper()
+print(salam)
+
+# merubah semua ke lowercase
+salam = salam.lower()
+print(salam)
+
+
+# # Pengecekan dengan isX method
+
+# contoh pengecekan lowercase
+is_lower = salam.islower()  # hasilnya boolean
+print(str(is_lower))
+
+# contoh pengecekan uppercase
+is_upper = salam.isupper()  # hasilnya boolean
+print(str(is_upper))
+
+# isalpha() => mengecek apakah semuanya huruf
+alpha = "hallo"
+print(alpha.isalpha())
+
+# isalnum() => apakah huruf dan angka
+alnum = "halo123"
+print(alnum.isalnum())
+
+# isdecimal() => apakah angka saja
+deci = "123"
+print(deci.isdecimal())
+
+# isspace() => apakah spasi, tab, newline
+space = "       \n"
+print(space.isspace())
+
+# istitle() => apakah semua kata dimulai dengan huruf besar
+title = "Nama Saya Yazid"
+print(title.istitle())
+
+# # ngecek kompoonen startswith() endswith() -> dimulai kata dan diangkiri kata
+cek_start = "Sangjangnim Oppa".startswith("Sangjangnim")
+print(cek_start)
+cek_end = "Sangjangnim Oppa".endswith("Oppa")
+print(cek_end)
+
+
+# # penggabungan komponen join() split()
+pisah = ['yazid', 'dhiaulhaq', 'ismail']
+gabung = '-'.join(pisah)
+print(gabung)  # jadi 1 dengan digabung -
+print(gabung.split('-'))  # kembali jadi list
+
+# # alokasi karakter rjust(), ljust(), center()
+kanan = "kanan".rjust(20, '-')
+print(kanan)
+
+kiri = "kiri".ljust(20)
+print(kiri)
+
+tengah = "tengah".center(20)
+print(tengah)
+
+# kebalikan dari alokasi karakter -> strip()
+kanan = kanan.strip('-')  # menghilangkan tanpa -
+print(kanan)
