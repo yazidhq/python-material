86d5426cf50ef5c93f46c16b08fe51cdf3529991
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-10-13 19:00:07 +0700

    If Elif Else Statement

---
+++ 15.if_else.py
@@ -0,0 +1,29 @@
+# if dan else statement
+
+#  1. ifnya
+#  2. kondisinya
+#  3. aksinya
+
+nama = input("Siapa nama anda?")
+
+# if statement
+if nama == "yazid":
+    print(f"Nama anda {nama}")
+
+print(f"Terimakasih {nama}")
+
+# if else statement
+if nama == "yazid":
+    print(f"Nama anda {nama}")
+else:
+    print(f"Kamu bukan yazid")
+print("Akhir Program")
+
+# if else if (elif) statement
+if nama == "yazid":
+    print(f"Nama anda {nama}")
+elif nama == "otong":
+    print(f"Nama anda {nama}")
+else:
+    print(f"Kamu bukan yazid/ otong")
+print("Akhir Program")
