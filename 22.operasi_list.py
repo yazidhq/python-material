5c4b287eaf5f1193d20991665adc7efce15a8116
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-10-17 12:12:27 +0700

    Operasi Manipulasi List

---
+++ 22.operasi_list.py
@@ -0,0 +1,17 @@
+data_angka = [1, 5, 3, 1, 5, 4, 3, 2, 7, 9]
+
+print(data_angka)
+
+# jumlah data
+print(data_angka.count(1))
+
+# ambil posisi data keberapa
+print(data_angka.index(9))
+
+# mengurutkan list
+data_angka.sort()
+print(data_angka)
+
+# membalikan listnya
+data_angka.reverse()
+print(data_angka)
