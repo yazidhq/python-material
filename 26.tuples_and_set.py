e5451302c8e736a3bb6c08a5ef15090a5a705193
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-10-20 20:30:21 +0700

    List, Tuple, Sets

---
+++ 26.tuples_and_set.py
@@ -0,0 +1,10 @@
+# list
+data_list = [0, 1, 2, 3, 4, 5]
+
+# tuples: (nialai konstan)
+# - tidak bisa di assignment atau diubah
+# - tidak bisa di apppend
+data_tuples = (0, 1, 2, 3, 4, 5)
+
+# sets: koneksi tanpa index (kumpulan data(himpunan))
+daata_sets = {0, 1, 2, 4, 5, 6}
