f5ced102823bbc6f2b7fdc4752c758a661e702fe
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-02 11:25:17 +0700

    Package Pygame

---
+++ pip/pygame_app.py
@@ -0,0 +1,53 @@
+import pygame
+
+# # init
+pygame.init()
+
+# Variabel running game
+isRun = True
+
+# membuat display surface object
+window_lebar = 500
+window_panjang = 500
+window = pygame.display.set_mode((window_lebar, window_panjang))
+
+# object game
+# - kordinat atau posisi
+x = 250
+y = 250
+# - ukuran
+panjang = 20
+lebar = 20
+# - kecepatan gerak
+speed = 5
+
+while isRun:
+    pygame.time.delay(10)
+
+    # # user input, database input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRun = False
+
+    # ambil semua keyboard press
+    keys = pygame.key.get_pressed()
+
+    # ambil ke kiri dan kanan (x)
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= speed
+    if keys[pygame.K_RIGHT] and x < window_lebar-lebar:
+        x += speed
+    # ambil ke atas dan bawah (y)
+    if keys[pygame.K_UP] and y > 0:
+        y -= speed
+    if keys[pygame.K_DOWN] and y < window_panjang-lebar:
+        y += speed
+
+    # # update asset
+    window.fill((255, 255, 255))
+    pygame.draw.rect(window, (255, 120, 0), (x, y, lebar, panjang))
+
+    # # render ke display
+    pygame.display.update()
+
+pygame.quit()
